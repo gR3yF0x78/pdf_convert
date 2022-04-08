3a2f7be87061c7115e4597782375f21a8fd6246b
gR3yF0x78 <simonfoxparker@protonmail.com>
2022-04-08 22:29:27 +0100

    Comment update

--- pdf_convert_to_png.py
+++ pdf_convert_to_png.py
@@ -1,8 +1,6 @@
 '''
 PDF to PNG - SP - 03/04/2022
-User system dependincies must be installed
-User selects PDF file to convert
-User selects output folder
+User system dependencies must be installed.
 '''
 
 import fitz # For PDF to PNG conversion
